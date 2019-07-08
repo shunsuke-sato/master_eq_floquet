2d8c8c40a7f9662f6105e05e7c022ca52d2c479b
Shunsuke Sato <shunsuke.sato26@gmail.com>
2019-07-08 17:40:18 +0200

    Added global variables.

---
+++ src/global_variables.f90
@@ -0,0 +1,11 @@
+module global_variables
+  implicit none
+! Math parameters
+  real(8),parameter :: pi = 4d0*atan(1d0)
+  complex(8),parameter :: zi = (0d0, 1d0)
+
+
+
+
+
+end module global_variables
